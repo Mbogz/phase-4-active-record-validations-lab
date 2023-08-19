2d1014236c868628025ef37f532ec919f092c745
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-08-19 11:46:11 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: { minimum:250 }
+    validates :summary, length: { maximum:250 }
+    validates :category, inclusion: { in: %w(Fictio Non-Fiction) }
+    validate :clickbait
+
+    CLICKBAITS = [ /Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i ]
+
 end
